92aae06d21b4252cab42e4a32588d6609caa365f
aurore <aurore.adrian@live.fr>
2019-04-03 11:54:23 +0200

    Mise à jour des commentaires

---
+++ hello.py
@@ -0,0 +1,14 @@
+"""
+Algorithm #0.0.1
+Declare var
+Store come arithmetics into the var 
+Displays result
+Operands and Operators, functions
+"""
+resultat = 0 # Définition de la variable
+resultat = 2 + 2
+print (resultat)
+
+"""
+Fin de l'algorithme
+"""
